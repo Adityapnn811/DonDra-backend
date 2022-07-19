d07108f09cd2afe1ca9f947e334630c06925b0dc
Adityapnn811 <72344203+Adityapnn811@users.noreply.github.com>
2022-07-19 07:45:21 +0700

    Added some package

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const port = process.env.PORT;
+const port = process.env.PORT || 3000;
 const app = express();
 
 const custRouters = require('./routes/customers');
@@ -7,6 +7,11 @@ const custRouters = require('./routes/customers');
 app.get("/", (req, res) => {
     res.send("Berhasil deploy");
 })
+
+app.get("/test", (_req, res) => {
+  res.status(200).send("Endpoint success");
+})
+
 
 app.use('/customers', custRouters);
 
