07d6dde96c70643b94bbc53a1db8dff227ec378d
Adityapnn811 <72344203+Adityapnn811@users.noreply.github.com>
2022-07-29 13:06:37 +0700

    Changed cors

--- index.ts
+++ index.ts
@@ -17,6 +17,24 @@ const getUnverifiedUsersRouters = require('./routes/getUnverifiedUsers');
 app.use(jsonParser)
 
 // cors options for front end
+app.use(function (req, res, next) {
+
+  // Website you wish to allow to connect
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000, https://dondra.vercel.app/');
+
+  // Request methods you wish to allow
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+  // Request headers you wish to allow
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+  // Set to true if you need the website to include cookies in the requests sent
+  // to the API (e.g. in case you use sessions)
+  res.setHeader('Access-Control-Allow-Credentials', true);
+
+  // Pass to next layer of middleware
+  next();
+});
 const corsOptions = {
   origin: ['https://dondra.vercel.app', 'http://localhost:3000'],
   methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
